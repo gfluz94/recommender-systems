64ac2ed61b38f6b15dab984976fb326cdef09bef
Gabriel Fernandes Luz <52358345+gfluz94@users.noreply.github.com>
2022-12-28 15:59:50 -0300

    add docstring and update errors raised in function

--- recsys/utils/aws.py
+++ recsys/utils/aws.py
@@ -1,16 +1,28 @@
 import os
 import boto3
 
+from recsys.utils.errors import EnvironmentVariablesMissing, S3ClientError
+
 SUCCESS_STATUS_CODE = 200
+
 
 def fetch_s3_files(
     bucket_name: str,
     target_folder: str
 ) -> None:
+    """Function that fetches data from specified S3 bucket and saves it to the target folder.
+    
+        Args:
+            bucket_name (str): Name of the bucket in S3 where files are nested
+            target_folder (str): Patht to folder where data is going to be saved.
+    
+        Raises:
+            SolverUnsuccessful: In case optimization encounters a problem along the way.
+    """
     ACCESS_KEY = os.getenv(key="AWS_ACCESS_KEY")
     SECRET_KEY = os.getenv(key="AWS_SECRET_KEY")
     if not ACCESS_KEY or not SECRET_KEY:
-        raise ValueError(
+        raise EnvironmentVariablesMissing(
           "AWS Credentials not set in environment variables `ACCESS_KEY` and `SECRET_KEY`"
         )
         client = boto3.client(
@@ -18,7 +30,7 @@ def fetch_s3_files(
         )
     content_response = client.list_objects(Bucket=bucket_name)
     if content_response["ResponseMetadata"]["HTTPStatusCode"] != SUCCESS_STATUS_CODE:
-        raise ValueError(
+        raise S3ClientError(
           "Please check either bucket name or permissions to access S3 bucket!"
         )
 
