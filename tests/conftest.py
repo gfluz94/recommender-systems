bc486e288852c7fa9073742eba40e815983f1093
gabriel <gfluz94@gmail.com>
2023-03-23 16:12:05 -0300

    add unit tests for UserUserCollaborativeFiltering

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,3 +20,25 @@ def dummy_ratings_df() -> pd.DataFrame:
 def movielens_sample() -> pd.DataFrame:
     wdir = os.path.abspath(os.curdir)
     return pd.read_csv(os.path.join(wdir, "tests", "movielens_sample.csv"))
+
+
+@pytest.fixture(scope="module")
+def output_predictions_for_user_user_filtering() -> pd.DataFrame:
+    return pd.DataFrame(
+        {
+            "movieId": {
+                250: 7153,
+                70: 267,
+                322: 7380,
+                69: 4361,
+                462: 4022,
+            },
+            "predicted_rating": {
+                250: 5.666849816849817,
+                70: 5.166849816849817,
+                322: 5.166849816849817,
+                69: 5.166849816849817,
+                462: 5.166849816849817,
+            },
+        }
+    )
