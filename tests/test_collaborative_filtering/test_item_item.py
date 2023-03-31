3faf76c6abb37f526ede652576525d60e6a150d9
gabriel <gfluz94@gmail.com>
2023-03-31 16:11:14 -0300

    adjust unit tests for ItemItemCollaborativeFiltering

--- tests/test_collaborative_filtering/test_item_item.py
+++ tests/test_collaborative_filtering/test_item_item.py
@@ -15,8 +15,8 @@ class TestItemItemCollaborativeFiltering(object):
     _USER_COL = "userId"
     _MOVIE_COL = "movieId"
     _RATING_COL = "rating"
-    _MINIMUM_COMMON_ITEMS = 5
-    _TOP_SIMILAR_USERS = 5
+    _MINIMUM_COMMON_USERS = 1
+    _TOP_SIMILAR_ITEMS = 3
 
     def test___init__RaisesSimilarityMethodNotAvailable(self):
         with pytest.raises(SimilarityMethodNotAvailable):
@@ -25,8 +25,8 @@ class TestItemItemCollaborativeFiltering(object):
                 item_id_field_name=self._MOVIE_COL,
                 rating_field_name=self._RATING_COL,
                 similarity_method="SEP99",
-                minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-                K_top_similar_items=self._TOP_SIMILAR_USERS,
+                minimum_common_users=self._MINIMUM_COMMON_USERS,
+                K_top_similar_items=self._TOP_SIMILAR_ITEMS,
             )
 
     def test___init__RaisesSimilarityMethodRequiresRating(self):
@@ -35,8 +35,8 @@ class TestItemItemCollaborativeFiltering(object):
                 user_id_field_name=self._USER_COL,
                 item_id_field_name=self._MOVIE_COL,
                 similarity_method="correlation",
-                minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-                K_top_similar_items=self._TOP_SIMILAR_USERS,
+                minimum_common_users=self._MINIMUM_COMMON_USERS,
+                K_top_similar_items=self._TOP_SIMILAR_ITEMS,
             )
 
         with pytest.raises(SimilarityMethodRequiresRating):
@@ -44,34 +44,34 @@ class TestItemItemCollaborativeFiltering(object):
                 user_id_field_name=self._USER_COL,
                 item_id_field_name=self._MOVIE_COL,
                 similarity_method="mse",
-                minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-                K_top_similar_items=self._TOP_SIMILAR_USERS,
+                minimum_common_users=self._MINIMUM_COMMON_USERS,
+                K_top_similar_items=self._TOP_SIMILAR_ITEMS,
             )
 
-    def test_fitRunsCorrectlyWithCorrelation(self, movielens_sample: pd.DataFrame):
+    def test_fitRunsCorrectlyWithCorrelation(self):
         # OUTPUT
         model = ItemItemCollaborativeFiltering(
             user_id_field_name=self._USER_COL,
             item_id_field_name=self._MOVIE_COL,
             rating_field_name=self._RATING_COL,
             similarity_method="correlation",
-            minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-            K_top_similar_items=self._TOP_SIMILAR_USERS,
+            minimum_common_users=self._MINIMUM_COMMON_USERS,
+            K_top_similar_items=self._TOP_SIMILAR_ITEMS,
         )
-        model.fit(movielens_sample)
+        input_sample = pd.DataFrame(
+            {
+                "userId": [1, 1, 2, 2, 3, 3, 4, 4],
+                "movieId": [1, 2, 1, 3, 2, 4, 3, 4],
+                "rating": [5, 5, 5, 4, 1, 5, 3, 5],
+            }
+        )
+        model.fit(input_sample)
 
         # EXPECTED
-        expected_user_similarity = {
-            1: [3196, 44950, 42181, 1284, 670],
-            670: [1, 42181, 44950],
-            1284: [1, 42181, 3196],
-            3196: [1, 42181, 44950, 1284],
-            42181: [1, 1284, 670, 44950, 3196],
-            44950: [1, 42181, 3196, 670],
-        }
+        expected_item_similarity = {1: [2, 3], 2: [1, 4], 3: [1, 4], 4: [2, 3]}
 
         # ASSERT
-        model._user_similarity == expected_user_similarity
+        model._item_similarity == expected_item_similarity
 
     def test_fitRunsCorrectlyWithMSE(self, movielens_sample: pd.DataFrame):
         # OUTPUT
@@ -80,23 +80,23 @@ class TestItemItemCollaborativeFiltering(object):
             item_id_field_name=self._MOVIE_COL,
             rating_field_name=self._RATING_COL,
             similarity_method="mse",
-            minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-            K_top_similar_items=self._TOP_SIMILAR_USERS,
+            minimum_common_users=self._MINIMUM_COMMON_USERS,
+            K_top_similar_items=self._TOP_SIMILAR_ITEMS,
         )
-        model.fit(movielens_sample)
+        input_sample = pd.DataFrame(
+            {
+                "userId": [1, 1, 2, 2, 3, 3, 4, 4],
+                "movieId": [1, 2, 1, 3, 2, 4, 3, 4],
+                "rating": [5, 5, 5, 4, 1, 5, 3, 5],
+            }
+        )
+        model.fit(input_sample)
 
         # EXPECTED
-        expected_user_similarity = {
-            1: [42181, 670, 1284, 3196, 44950],
-            670: [1, 42181, 44950],
-            1284: [42181, 1, 3196],
-            3196: [1, 42181, 1284, 44950],
-            42181: [1, 1284, 670, 44950, 3196],
-            44950: [1, 42181, 670, 3196],
-        }
+        expected_item_similarity = {1: [3, 2], 2: [1, 4], 3: [1, 4], 4: [3, 2]}
 
         # ASSERT
-        model._user_similarity == expected_user_similarity
+        model._item_similarity == expected_item_similarity
 
     def test_fitRunsCorrectlyWithIoUAndWithoutRatings(
         self, movielens_sample: pd.DataFrame
@@ -106,31 +106,31 @@ class TestItemItemCollaborativeFiltering(object):
             user_id_field_name=self._USER_COL,
             item_id_field_name=self._MOVIE_COL,
             similarity_method="IoU",
-            minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-            K_top_similar_items=self._TOP_SIMILAR_USERS,
+            minimum_common_users=self._MINIMUM_COMMON_USERS,
+            K_top_similar_items=self._TOP_SIMILAR_ITEMS,
         )
-        model.fit(movielens_sample)
+        input_sample = pd.DataFrame(
+            {
+                "userId": [1, 1, 2, 2, 3, 3, 4, 4],
+                "movieId": [1, 2, 1, 3, 2, 4, 3, 4],
+                "rating": [5, 5, 5, 4, 1, 5, 3, 5],
+            }
+        )
+        model.fit(input_sample)
 
         # EXPECTED
-        expected_user_similarity = {
-            1: [670, 1284, 3196, 42181, 44950],
-            670: [44950, 1, 42181],
-            1284: [1, 3196, 42181],
-            3196: [1, 1284, 42181, 44950],
-            42181: [1, 670, 1284, 3196, 44950],
-            44950: [670, 1, 3196, 42181],
-        }
+        expected_item_similarity = {1: [2, 3], 2: [1, 4], 3: [1, 4], 4: [2, 3]}
 
         # ASSERT
-        model._user_similarity == expected_user_similarity
+        model._item_similarity == expected_item_similarity
 
     def test_predictRaisesModelNotFittedYet(self):
         model = ItemItemCollaborativeFiltering(
             user_id_field_name=self._USER_COL,
             item_id_field_name=self._MOVIE_COL,
             similarity_method="IoU",
-            minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-            K_top_similar_items=self._TOP_SIMILAR_USERS,
+            minimum_common_users=self._MINIMUM_COMMON_USERS,
+            K_top_similar_items=self._TOP_SIMILAR_ITEMS,
         )
         with pytest.raises(ModelNotFittedYet):
             model.predict(user=1)
@@ -141,36 +141,49 @@ class TestItemItemCollaborativeFiltering(object):
             item_id_field_name=self._MOVIE_COL,
             rating_field_name=self._RATING_COL,
             similarity_method="correlation",
-            minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-            K_top_similar_items=self._TOP_SIMILAR_USERS,
+            minimum_common_users=self._MINIMUM_COMMON_USERS,
+            K_top_similar_items=self._TOP_SIMILAR_ITEMS,
         )
         model.fit(movielens_sample)
         with pytest.raises(UserNotPresent):
             model.predict(user=20202021)
 
-    def test_predictRunsCorrectlyWithDataFrame(self, movielens_sample: pd.DataFrame):
+    def test_predictRunsCorrectlyWithDataFrame(self):
         # OUTPUT
         model = ItemItemCollaborativeFiltering(
             user_id_field_name=self._USER_COL,
             item_id_field_name=self._MOVIE_COL,
             rating_field_name=self._RATING_COL,
             similarity_method="correlation",
-            minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-            K_top_similar_items=self._TOP_SIMILAR_USERS,
+            minimum_common_users=self._MINIMUM_COMMON_USERS,
+            K_top_similar_items=self._TOP_SIMILAR_ITEMS,
         )
-        model.fit(movielens_sample)
-        output = model.predict(
-            df=pd.DataFrame(
-                [{"userId": 1, "movieId": 4022}, {"userId": 1, "movieId": 37729}]
-            )
+        input_sample = pd.DataFrame(
+            {
+                "userId": [1, 1, 2, 2, 3, 3, 4, 4],
+                "movieId": [1, 2, 1, 3, 2, 4, 3, 4],
+                "rating": [5, 5, 5, 4, 1, 5, 3, 5],
+            }
         )
+        model.fit(input_sample)
+        output = model.predict(input_sample)
 
         # EXPECTED
         expected = pd.DataFrame(
             {
-                "userId": {0: 1, 1: 1},
-                "movieId": {0: 4022, 1: 37729},
-                "predicted_rating": {0: 5.166849816849817, 1: 0.5979736575481258},
+                "userId": {0: 1, 1: 1, 2: 2, 3: 2, 4: 3, 5: 3, 6: 4, 7: 4},
+                "movieId": {0: 1, 1: 2, 2: 1, 3: 3, 4: 2, 5: 4, 6: 3, 7: 4},
+                "rating": {0: 5, 1: 5, 2: 5, 3: 4, 4: 1, 5: 5, 6: 3, 7: 5},
+                "predicted_rating": {
+                    0: 7.0,
+                    1: 5.0,
+                    2: 3.5,
+                    3: 3.0,
+                    4: 3.5,
+                    5: 1.5,
+                    6: 5.0,
+                    7: 4.5,
+                },
             }
         )
 
@@ -179,8 +192,6 @@ class TestItemItemCollaborativeFiltering(object):
 
     def test_predictRunsCorrectlyWithUser(
         self,
-        movielens_sample: pd.DataFrame,
-        output_predictions_for_user_user_filtering: pd.DataFrame,
     ):
         # OUTPUT
         model = ItemItemCollaborativeFiltering(
@@ -188,14 +199,21 @@ class TestItemItemCollaborativeFiltering(object):
             item_id_field_name=self._MOVIE_COL,
             rating_field_name=self._RATING_COL,
             similarity_method="correlation",
-            minimum_common_users=self._MINIMUM_COMMON_ITEMS,
-            K_top_similar_items=self._TOP_SIMILAR_USERS,
+            minimum_common_users=self._MINIMUM_COMMON_USERS,
+            K_top_similar_items=self._TOP_SIMILAR_ITEMS,
         )
-        model.fit(movielens_sample)
-        output = model.predict(user=1, K_top_items=5)
+        input_sample = pd.DataFrame(
+            {
+                "userId": [1, 1, 2, 2, 3, 3, 4, 4],
+                "movieId": [1, 2, 1, 3, 2, 4, 3, 4],
+                "rating": [5, 5, 5, 4, 1, 5, 3, 5],
+            }
+        )
+        model.fit(input_sample)
+        output = model.predict(user=1, K_top_items=2)
 
         # EXPECTED
-        expected = output_predictions_for_user_user_filtering.copy()
+        expected = pd.DataFrame({"movieId": {0: 3}, "predicted_rating": {0: 5.0}})
 
         # ASSERT
         pd.testing.assert_frame_equal(output, expected)
