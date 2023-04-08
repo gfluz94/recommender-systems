50fcbc8408a579d3f3eb335e0f5da292700f12f6
gabriel <gfluz94@gmail.com>
2023-04-08 18:19:24 -0300

    make ALS importable from matrix_factorization module

--- recsys/matrix_factorization/__init__.py
+++ recsys/matrix_factorization/__init__.py
@@ -1,2 +1,3 @@
 from recsys.matrix_factorization.data import *
 from recsys.matrix_factorization.embedding import *
+from recsys.matrix_factorization.als import *
--- recsys/matrix_factorization/als.py
+++ recsys/matrix_factorization/als.py
@@ -1,3 +1,5 @@
+__all__ = ["ALS"]
+
 from typing import Dict, List, Optional
 import numpy as np
 import pandas as pd
